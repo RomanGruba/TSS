93e81c126caeff5facd88113d45f3fa02e776036
Roman Gruba <roman.gruba@gmail.com>
2020-04-25 13:24:19 +0300

    theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 
 import { unregister } from "./core";
+import { GlobalStyles, theme } from "./styles";
 
 ReactDOM.render(
-  <div className="">Hello TS</div>,
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <div className="">Hello TS</div>
+  </ThemeProvider>,
   document.getElementById("root")
 );
 
