30177e55df85e09c36d3105bcb838d8630c83d89
Roman Gruba <roman.gruba@gmail.com>
2020-04-26 12:37:24 +0300

    shuffle

--- src/utils/fill-grid/index.ts
+++ src/utils/fill-grid/index.ts
@@ -1,4 +1,9 @@
-const gridExample = [
+import { GRID, NUMBERS } from "typings";
+import { shuffle } from "utils";
+
+const numbers: NUMBERS[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const gridExample: GRID = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -10,6 +15,21 @@ const gridExample = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
-function fillGrid(grid: any) {}
+function fillGrid(grid: GRID) {
+  let row = 0;
+  let col = 0;
+
+  for (let i = 0; i < 81; i++) {
+    row = Math.floor(i / 9);
+    col = i % 9;
+
+    if (grid[row][col] === 0) {
+      shuffle(numbers);
+      break;
+    }
+  }
+
+  grid[row][col] = 0;
+}
 
 export default fillGrid;
