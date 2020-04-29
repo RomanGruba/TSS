baf87f32eb7493d08de90883ef53236a00317de4
Roman Gruba <roman.gruba@gmail.com>
2020-04-30 00:14:38 +0300

    copyGrid

---
+++ src/utils/get-random-index/index.ts
@@ -0,0 +1,5 @@
+function getRandom() {
+  return Math.floor(Math.random() * 9);
+}
+
+export default getRandom;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,5 @@ export * from "./is-in";
 export { default as identifySquare } from "./identify-square";
 export { default as checkGrid } from "./check-grid";
 export { default as createFullGrid } from "./create-full-grid";
+export { default as getRandomIndex } from "./get-random-index";
+export { default as copyGrid } from "./copy-grid";
